33d50a6f2ec219ab0d2d15eb5bec16ddcdce616e
jon <jon.zhao@hdchain.io>
2018-07-26 14:29:45 +0800

    [modified]: add syncmode=full
    1.

--- Geth/d1.bat
+++ Geth/d1.bat
@@ -11,7 +11,7 @@ set acc=0xd6bd66ad9ab4a451963b30479a5cb3a9217a1b83
 geth --datadir d1 ^
 --bootnodes enode://d9840cf88e3880ae4ae1162e3d608e03b07e33e8509972104de434a9daa4a95cc8911b203fa4fc988399511552b2caeb3435c6b3b644bfdd49374f2a51b1a85a@127.0.0.1:30301 ^
 --txpool.globalslots 1000000 ^
---gcmode archive ^
+--gcmode archive --syncmode=full ^
 --targetgaslimit 9223372036854775808 ^
 --rpc --rpcapi eth,net,web3,personal ^
 --gasprice 0 ^
--- Geth/d5.bat
+++ Geth/d5.bat
@@ -11,7 +11,7 @@ set acc=0x62f7a6054f347c114df2158458e95128657ed5b4
 geth --datadir d5 ^
 --bootnodes enode://d9840cf88e3880ae4ae1162e3d608e03b07e33e8509972104de434a9daa4a95cc8911b203fa4fc988399511552b2caeb3435c6b3b644bfdd49374f2a51b1a85a@127.0.0.1:30301 ^
 --txpool.globalslots 1000000 ^
---gcmode archive ^
+--gcmode archive --syncmode=full ^
 --rpc --rpcapi eth,net,web3,personal ^
 --targetgaslimit 9223372036854775808 ^
 --gasprice 0 ^
