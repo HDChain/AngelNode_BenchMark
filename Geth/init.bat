4652d4d8700a38c2d7281ea387c2b8ee9eb63011
jon <jon.zhao@hdchain.io>
2018-07-26 12:12:39 +0800

    [modified]: add gcmode to fix node state lost
    1.

--- Geth/init.bat
+++ Geth/init.bat
@@ -11,15 +11,9 @@ geth --datadir d2 init .puppeth\genesis.json
 geth --datadir d3 init .puppeth\genesis.json
 geth --datadir d4 init .puppeth\genesis.json
 geth --datadir d5 init .puppeth\genesis.json
-geth --datadir d6 init .puppeth\genesis.json
-geth --datadir d7 init .puppeth\genesis.json
-geth --datadir d8 init .puppeth\genesis.json
 
 copy keystore\* d1\keystore
 copy keystore\* d2\keystore
 copy keystore\* d3\keystore
 copy keystore\* d4\keystore
 copy keystore\* d5\keystore
-copy keystore\* d6\keystore
-copy keystore\* d7\keystore
-copy keystore\* d8\keystore
